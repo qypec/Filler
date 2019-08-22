93a89dc6e2af9e9746eb1e92b5863937d766e19e
qypec <qypec.prog@yandex.ru>
2019-08-22 17:26:39 +0300

    added libft and base of project

---
+++ includes/filler.mk
@@ -0,0 +1,8 @@
+# Base variables for minishell
+
+CC = gcc
+CFLAGS = -g -c -Wall -Wextra -Werror
+FILLER_H = -I ../includes/
+COMPILING = $(CC) $(CFLAGS) $(FILLER_H)
+EXECUTABLE = yquaro.filler
+LIBNAME = libft.a
